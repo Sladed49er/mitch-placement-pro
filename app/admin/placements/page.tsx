92ea6ca36abae09ccd8dd57a62281d21bf2b6825
Sladed49er <matt@everysolutionit.com>
2025-09-03 11:38:43 -0700

    Fix missing signOut import in admin pages

--- app/admin/placements/page.tsx
+++ app/admin/placements/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
@@ -151,3 +151,4 @@ export default function AdminPlacementsPage() {
     </div>
   );
 }
+
